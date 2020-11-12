75ec18ad8ce4b6e881515d401d381b24dcf223b3
Joey-hage <joeyhage2000@gmail.com>
2020-11-12 16:34:53 +0100

    base

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {LogInRoutingModule} from './connection/log-in/log-in-routing.module';
+import {SignInRoutingModule} from './connection/sign-in/sign-in-routing.module';
+
+@NgModule({
+  declarations: [AppRoutingModule.components],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        LogInRoutingModule,
+        SignInRoutingModule,
+        AppRoutingModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
